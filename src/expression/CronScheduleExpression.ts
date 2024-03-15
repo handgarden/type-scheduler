89f3f3dbd04c4bf1d92d534c5912b2602344c143
handgarden <sjungwon03@gmail.com>
2024-03-16 01:12:36 +0900

    feat: CronScheduleExpression

--- src/expression/CronScheduleExpression.ts
+++ src/expression/CronScheduleExpression.ts
@@ -1,84 +1,90 @@
 import { CronField } from "./CronField";
 
 export class CronScheduleExpression {
-  private readonly _minute: CronField = CronField.EVERY;
-  private readonly _hour: CronField = CronField.EVERY;
-  private readonly _dayOfMonth: CronField = CronField.EVERY;
-  private readonly _month: CronField = CronField.EVERY;
-  private readonly _dayOfWeek: CronField = CronField.EVERY;
+  private _minute: CronField = CronField.EVERY;
+  private _hour: CronField = CronField.EVERY;
+  private _dayOfMonth: CronField = CronField.EVERY;
+  private _month: CronField = CronField.EVERY;
+  private _dayOfWeek: CronField = CronField.EVERY;
 
-  private constructor({
-    minute,
-    hour,
-    dayOfMonth,
-    month,
-    dayOfWeek,
-  }: {
-    minute: CronField;
-    hour: CronField;
-    dayOfMonth: CronField;
-    month: CronField;
-    dayOfWeek: CronField;
-  }) {
+  minute(minute: CronField): this {
     this._minute = minute;
-    this._hour = hour;
-    this._dayOfMonth = dayOfMonth;
-    this._month = month;
-    this._dayOfWeek = dayOfWeek;
+    return this;
   }
 
-  toString(): string {
+  everyStepMinute(step: number): this {
+    this._minute = CronField.EVERY_STEP(step);
+    return this;
+  }
+
+  rangeMinute(start: number, end: number): this {
+    this._minute = CronField.RANGE(start, end);
+    return this;
+  }
+
+  hour(hour: CronField): this {
+    this._hour = hour;
+    return this;
+  }
+
+  everyStepHour(step: number): this {
+    this._hour = CronField.EVERY_STEP(step);
+    return this;
+  }
+
+  rangeHour(start: number, end: number): this {
+    this._hour = CronField.RANGE(start, end);
+    return this;
+  }
+
+  dayOfMonth(dayOfMonth: CronField): this {
+    this._dayOfMonth = dayOfMonth;
+    return this;
+  }
+
+  everyStepDayOfMonth(step: number): this {
+    this._dayOfMonth = CronField.EVERY_STEP(step);
+    return this;
+  }
+
+  rangeDayOfMonth(start: number, end: number): this {
+    this._dayOfMonth = CronField.RANGE(start, end);
+    return this;
+  }
+
+  month(month: CronField): this {
+    this._month = month;
+    return this;
+  }
+
+  everyStepMonth(step: number): this {
+    this._month = CronField.EVERY_STEP(step);
+    return this;
+  }
+
+  rangeMonth(start: number, end: number): this {
+    this._month = CronField.RANGE(start, end);
+    return this;
+  }
+
+  dayOfWeek(dayOfWeek: CronField): this {
+    this._dayOfWeek = dayOfWeek;
+    return this;
+  }
+
+  everyStepDayOfWeek(step: number): this {
+    this._dayOfWeek = CronField.EVERY_STEP(step);
+    return this;
+  }
+
+  rangeDayOfWeek(start: number, end: number): this {
+    this._dayOfWeek = CronField.RANGE(start, end);
+    return this;
+  }
+
+  build(): string {
     return `${this._minute} ${this._hour} ${this._dayOfMonth} ${this._month} ${this._dayOfWeek}`;
   }
 
-  static EVERT_MINUTE = new CronScheduleExpression({
-    minute: "*",
-    hour: "*",
-    dayOfMonth: "*",
-    month: "*",
-    dayOfWeek: "*",
-  });
-
-  static Builder = class {
-    private _minute: CronField = "*";
-    private _hour: CronField = "*";
-    private _dayOfMonth: CronField = "*";
-    private _month: CronField = "*";
-    private _dayOfWeek: CronField = "*";
-
-    minute(minute: CronField): this {
-      this._minute = minute;
-      return this;
-    }
-
-    hour(hour: CronField): this {
-      this._hour = hour;
-      return this;
-    }
-
-    dayOfMonth(dayOfMonth: CronField): this {
-      this._dayOfMonth = dayOfMonth;
-      return this;
-    }
-
-    month(month: CronField): this {
-      this._month = month;
-      return this;
-    }
-
-    dayOfWeek(dayOfWeek: CronField): this {
-      this._dayOfWeek = dayOfWeek;
-      return this;
-    }
-
-    build(): CronScheduleExpression {
-      return new CronScheduleExpression({
-        minute: this._minute,
-        hour: this._hour,
-        dayOfMonth: this._dayOfMonth,
-        month: this._month,
-        dayOfWeek: this._dayOfWeek,
-      });
-    }
-  };
+  static EVERY_MINUTE = new CronScheduleExpression().build();
 }
