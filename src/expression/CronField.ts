ac86c69d64d64e24a8b878e9f0980c80734bfe4f
handgarden <sjungwon03@gmail.com>
2024-03-16 01:32:47 +0900

    fix: change EVERY_STEP to STEP

--- src/expression/CronField.ts
+++ src/expression/CronField.ts
@@ -1,8 +1,6 @@
 export class CronField {
   static readonly EVERY = "*";
-  static readonly EVERY_STEP = (step: number) => `*/${step}`;
   static readonly LIST = (values: (string | number)[]) => values.join(",");
   static readonly RANGE = (from: number, to: number) => `${from}-${to}`;
-  static readonly STEP = (start: number, interval: number) =>
-    `${start}/${interval}`;
+  static readonly STEP = (step: number) => `*/${step}`;
 }
