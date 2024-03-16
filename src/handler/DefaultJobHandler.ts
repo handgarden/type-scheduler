59c6ce6d7efc58d1dd1fb48e83a5a61506a5d932
handgarden <sjungwon03@gmail.com>
2024-03-16 22:02:05 +0900

    feat: impl abstract JobHandler

---
+++ src/handler/DefaultJobHandler.ts
@@ -0,0 +1,16 @@
+import { CronExpression } from "../expression";
+import { JobHandler } from "./JobHandler";
+
+export abstract class DefaultJobHandler implements JobHandler {
+  private readonly _cronExpression: CronExpression;
+
+  constructor(cronExpression: CronExpression) {
+    this._cronExpression = cronExpression;
+  }
+
+  public get cronExpression(): string {
+    return this._cronExpression.build();
+  }
+
+  public abstract handle(): void | Promise<void>;
+}
