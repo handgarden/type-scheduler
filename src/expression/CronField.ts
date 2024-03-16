c662e32add7495c6fc903ea6427d7b7e4c229797
handgarden <sjungwon03@gmail.com>
2024-03-16 20:59:58 +0900

    feat: CronField VALUE, LIST

--- src/expression/CronField.ts
+++ src/expression/CronField.ts
@@ -1,6 +1,7 @@
 export class CronField {
   static readonly EVERY = "*";
-  static readonly LIST = (values: (string | number)[]) => values.join(",");
+  static readonly VALUE = (value: number) => value;
+  static readonly LIST = (values: number[]) => values.join(",");
   static readonly RANGE = (from: number, to: number) => `${from}-${to}`;
   static readonly STEP = (step: number) => `*/${step}`;
 }
