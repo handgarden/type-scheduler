eaab74a84f01790bc2af364483b7d70c87f2259d
handgarden <sjungwon03@gmail.com>
2024-03-24 00:21:25 +0900

    test: Add test case for throwing error when min is greater than max in RangeCronField

--- test/expression/field/RangeCronField.spec.ts
+++ test/expression/field/RangeCronField.spec.ts
@@ -1,8 +1,17 @@
+import { InvalidCronRangeError } from "../../../src/error/InvalidCronRangeError";
 import { RangeCronField } from "../../../src/expression/field/RangeCronField";
 
 describe(RangeCronField, () => {
   it("should return min, max input to `${min}-${max}` string", () => {
     const field = new RangeCronField(0, 5);
     expect(field.build()).toEqual("0-5");
+  });
+
+  it("should throw error when min is greater than max", () => {
+    try {
+      new RangeCronField(5, 0);
+    } catch (e) {
+      expect(e).toBeInstanceOf(InvalidCronRangeError);
+    }
   });
 });
