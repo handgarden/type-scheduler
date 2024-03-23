e88c9582d8e3ede18d80bb07add5a1f70b17e54e
handgarden <sjungwon03@gmail.com>
2024-03-24 00:23:24 +0900

    fix: Refactor CronExpression class to use CronFieldBuilderFactory

--- src/expression/CronExpression.ts
+++ src/expression/CronExpression.ts
@@ -1,161 +1,48 @@
-import { DayOfMonthOutOfRangeError } from "../error/DayOfMonthOutOfRangeError";
-import { DayOfWeekOutOfRangeError } from "../error/DayOfWeekOutOfRangeError";
-import { HourOutOfRangeError } from "../error/HourOutOfRangeError";
-import { MinuteOutOfRangeError } from "../error/MinuteOutOfRangeError";
-import { MonthOutOfRangeError } from "../error/MonthOutOfRangeError";
-import { ArrayUtils } from "../utils/ArrayUtils";
-import { CronField } from "./CronField";
+import { CronField } from "./field/CronField";
+import { CronFieldBuilderFactory } from "./field/CronFieldBuilderFactory";
+import { WildcardCronField } from "./field/WildcardCronField";
+import { CronFieldFactory } from "./field/CronFieldFactory";
 
 export class CronExpression {
-  private _minute: CronField = CronField.EVERY;
-  private _hour: CronField = CronField.EVERY;
-  private _dayOfMonth: CronField = CronField.EVERY;
-  private _month: CronField = CronField.EVERY;
-  private _dayOfWeek: CronField = CronField.EVERY;
+  private readonly builderFactory: CronFieldBuilderFactory =
+    new CronFieldBuilderFactory();
+  private _minute: CronField = new WildcardCronField();
+  private _hour: CronField = new WildcardCronField();
+  private _dayOfMonth: CronField = new WildcardCronField();
+  private _month: CronField = new WildcardCronField();
+  private _dayOfWeek: CronField = new WildcardCronField();
 
-  public minute(...minutes: number[]): this {
-    for (let minute of minutes) {
-      this.validateMinute(minute);
-    }
-
-    this._minute = CronField.LIST(ArrayUtils.unique(minutes));
-
+  public minute(factory: CronFieldFactory): this {
+    const minuteBuilder = this.builderFactory.minute();
+    this._minute = factory(minuteBuilder);
     return this;
   }
 
-  public stepMinute(step: number): this {
-    this._minute = CronField.STEP(step);
+  public hour(factory: CronFieldFactory): this {
+    const hourBuilder = this.builderFactory.hour();
+    this._hour = factory(hourBuilder);
     return this;
   }
 
-  public rangeMinute(start: number, end: number): this {
-    this.validateMinute(start);
-    this.validateMinute(end);
-
-    this._minute = CronField.RANGE(start, end);
+  public dayOfMonth(factory: CronFieldFactory): this {
+    const dayOfMonthBuilder = this.builderFactory.dayOfMonth();
+    this._dayOfMonth = factory(dayOfMonthBuilder);
     return this;
   }
 
-  public hour(...hours: number[]): this {
-    for (let hour of hours) {
-      this.validateHour(hour);
-    }
-
-    this._hour = CronField.LIST(ArrayUtils.unique(hours));
+  public month(factory: CronFieldFactory): this {
+    const monthBuilder = this.builderFactory.month();
+    this._month = factory(monthBuilder);
     return this;
   }
 
-  public stepHour(step: number): this {
-    this._hour = CronField.STEP(step);
-    return this;
-  }
-
-  public rangeHour(start: number, end: number): this {
-    this.validateHour(start);
-    this.validateHour(end);
-
-    this._hour = CronField.RANGE(start, end);
-    return this;
-  }
-
-  public dayOfMonth(...dayOfMonths: number[]): this {
-    for (let dayOfMonth of dayOfMonths) {
-      this.validateDayOfMonth(dayOfMonth);
-    }
-
-    this._dayOfMonth = CronField.LIST(ArrayUtils.unique(dayOfMonths));
-    return this;
-  }
-
-  public stepDayOfMonth(step: number): this {
-    this._dayOfMonth = CronField.STEP(step);
-    return this;
-  }
-
-  public rangeDayOfMonth(start: number, end: number): this {
-    this.validateDayOfMonth(start);
-    this.validateDayOfMonth(end);
-
-    this._dayOfMonth = CronField.RANGE(start, end);
-    return this;
-  }
-
-  public month(...months: number[]): this {
-    for (let month of months) {
-      this.validateMonth(month);
-    }
-
-    this._month = CronField.LIST(ArrayUtils.unique(months));
-    return this;
-  }
-
-  public stepMonth(step: number): this {
-    this._month = CronField.STEP(step);
-    return this;
-  }
-
-  public rangeMonth(start: number, end: number): this {
-    this.validateMonth(start);
-    this.validateMonth(end);
-
-    this._month = CronField.RANGE(start, end);
-    return this;
-  }
-
-  public dayOfWeek(...dayOfWeeks: number[]): this {
-    for (let dayOfWeek of dayOfWeeks) {
-      this.validateDayOfWeek(dayOfWeek);
-    }
-    this._dayOfWeek = CronField.LIST(ArrayUtils.unique(dayOfWeeks));
-    return this;
-  }
-
-  public stepDayOfWeek(step: number): this {
-    this._dayOfWeek = CronField.STEP(step);
-    return this;
-  }
-
-  public rangeDayOfWeek(start: number, end: number): this {
-    this.validateDayOfWeek(start);
-    this.validateDayOfWeek(end);
-
-    this._dayOfWeek = CronField.RANGE(start, end);
+  public dayOfWeek(factory: CronFieldFactory): this {
+    const dayOfWeekBuilder = this.builderFactory.dayOfWeek();
+    this._dayOfWeek = factory(dayOfWeekBuilder);
     return this;
   }
 
   public build(): string {
-    return `${this._minute} ${this._hour} ${this._dayOfMonth} ${this._month} ${this._dayOfWeek}`;
-  }
-
-  static EVERY_MINUTE = new CronExpression().build();
-
-  private validateMinute(minute: number) {
-    if (minute < 0 || minute > 59) {
-      throw new MinuteOutOfRangeError(minute);
-    }
-  }
-
-  private validateHour(hour: number) {
-    if (hour < 0 || hour > 23) {
-      throw new HourOutOfRangeError(hour);
-    }
-  }
-
-  private validateDayOfMonth(dayOfMonth: number) {
-    if (dayOfMonth < 1 || dayOfMonth > 31) {
-      throw new DayOfMonthOutOfRangeError(dayOfMonth);
-    }
-  }
-
-  private validateMonth(month: number) {
-    if (month < 1 || month > 12) {
-      throw new MonthOutOfRangeError(month);
-    }
-  }
-
-  private validateDayOfWeek(dayOfWeek: number) {
-    if (dayOfWeek < 0 || dayOfWeek > 6) {
-      throw new DayOfWeekOutOfRangeError(dayOfWeek);
-    }
+    return `${this._minute.build()} ${this._hour.build()} ${this._dayOfMonth.build()} ${this._month.build()} ${this._dayOfWeek.build()}`;
   }
 }
