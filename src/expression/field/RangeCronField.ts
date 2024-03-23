a8235069488d89071829ed6f4ffadfa4695056f3
handgarden <sjungwon03@gmail.com>
2024-03-24 00:21:15 +0900

    fix: Add validation for min and max values in RangeCronField constructor

--- src/expression/field/RangeCronField.ts
+++ src/expression/field/RangeCronField.ts
@@ -1,7 +1,16 @@
+import { InvalidCronRangeError } from "../../error/InvalidCronRangeError";
 import { CronField } from "./CronField";
 
 export class RangeCronField implements CronField {
-  constructor(public readonly min: number, public readonly max: number) {}
+  constructor(public readonly min: number, public readonly max: number) {
+    if (this.isMinGreaterThanMax()) {
+      throw new InvalidCronRangeError(min, max);
+    }
+  }
+  private isMinGreaterThanMax(): boolean {
+    return this.min > this.max;
+  }
+
   build() {
     return `${this.min}-${this.max}`;
   }
