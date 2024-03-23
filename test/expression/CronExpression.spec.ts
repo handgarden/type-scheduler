7a22f51dad654ea51dc8a38c2158ccf65758027d
handgarden <sjungwon03@gmail.com>
2024-03-24 00:24:22 +0900

    test: Refactor CronExpression class to use CronFieldBuilderFactory

--- test/expression/CronExpression.spec.ts
+++ test/expression/CronExpression.spec.ts
@@ -9,34 +9,42 @@ describe(CronExpression, () => {
   describe("build", () => {
     it("should create every minute", () => {
       const expression = new CronExpression().build();
-      expect(expression.toString()).toBe("* * * * *");
+      expect(expression).toBe("* * * * *");
     });
   });
 
   describe("minute", () => {
     it("should create every step minute", () => {
-      const expression = new CronExpression().stepMinute(5).build();
-      expect(expression.toString()).toBe("*/5 * * * *");
+      const expression = new CronExpression()
+        .minute((minute) => minute.step(5))
+        .build();
+      expect(expression).toBe("*/5 * * * *");
     });
 
     it("should create range minute", () => {
-      const expression = new CronExpression().rangeMinute(5, 10).build();
-      expect(expression.toString()).toBe("5-10 * * * *");
+      const expression = new CronExpression()
+        .minute((minute) => minute.range(5, 10))
+        .build();
+      expect(expression).toBe("5-10 * * * *");
     });
 
     it("should create list minute", () => {
-      const expression = new CronExpression().minute(5, 10, 15).build();
-      expect(expression.toString()).toBe("5,10,15 * * * *");
+      const expression = new CronExpression()
+        .minute((minute) => minute.list(5, 10, 15))
+        .build();
+      expect(expression).toBe("5,10,15 * * * *");
     });
 
     it("should create list minute with unique values", () => {
-      const expression = new CronExpression().minute(5, 10, 15, 15).build();
-      expect(expression.toString()).toBe("5,10,15 * * * *");
+      const expression = new CronExpression()
+        .minute((minute) => minute.list(5, 10, 15, 15))
+        .build();
+      expect(expression).toBe("5,10,15 * * * *");
     });
 
     it("should throw error for minute out of range", () => {
       try {
-        new CronExpression().minute(60).build();
+        new CronExpression().minute((minute) => minute.value(60)).build();
       } catch (e) {
         expect(e).toBeInstanceOf(MinuteOutOfRangeError);
       }
@@ -45,28 +53,36 @@ describe(CronExpression, () => {
 
   describe("hour", () => {
     it("should create every step hour", () => {
-      const expression = new CronExpression().stepHour(5).build();
+      const expression = new CronExpression()
+        .hour((hour) => hour.step(5))
+        .build();
       expect(expression.toString()).toBe("* */5 * * *");
     });
 
     it("should create range hour", () => {
-      const expression = new CronExpression().rangeHour(5, 10).build();
+      const expression = new CronExpression()
+        .hour((hour) => hour.range(5, 10))
+        .build();
       expect(expression.toString()).toBe("* 5-10 * * *");
     });
 
     it("should create list hour", () => {
-      const expression = new CronExpression().hour(5, 10, 15).build();
+      const expression = new CronExpression()
+        .hour((hour) => hour.list(5, 10, 15))
+        .build();
       expect(expression.toString()).toBe("* 5,10,15 * * *");
     });
 
     it("should create list hour with unique values", () => {
-      const expression = new CronExpression().hour(5, 10, 15, 15).build();
+      const expression = new CronExpression()
+        .hour((hour) => hour.list(5, 10, 15, 15))
+        .build();
       expect(expression.toString()).toBe("* 5,10,15 * * *");
     });
 
     it("should throw error for hour out of range", () => {
       try {
-        new CronExpression().hour(24).build();
+        new CronExpression().hour((hour) => hour.value(24)).build();
       } catch (e) {
         expect(e).toBeInstanceOf(HourOutOfRangeError);
       }
@@ -75,28 +91,38 @@ describe(CronExpression, () => {
 
   describe("dayOfMonth", () => {
     it("should create every step day of month", () => {
-      const expression = new CronExpression().stepDayOfMonth(5).build();
+      const expression = new CronExpression()
+        .dayOfMonth((dayOfMonth) => dayOfMonth.step(5))
+        .build();
       expect(expression.toString()).toBe("* * */5 * *");
     });
 
     it("should create range day of month", () => {
-      const expression = new CronExpression().rangeDayOfMonth(5, 10).build();
+      const expression = new CronExpression()
+        .dayOfMonth((dayOfMonth) => dayOfMonth.range(5, 10))
+        .build();
       expect(expression.toString()).toBe("* * 5-10 * *");
     });
 
     it("should create list day of month", () => {
-      const expression = new CronExpression().dayOfMonth(5, 10, 15).build();
+      const expression = new CronExpression()
+        .dayOfMonth((dayOfMonth) => dayOfMonth.list(5, 10, 15))
+        .build();
       expect(expression.toString()).toBe("* * 5,10,15 * *");
     });
 
     it("should create list day of month with unique values", () => {
-      const expression = new CronExpression().dayOfMonth(5, 10, 15, 15).build();
+      const expression = new CronExpression()
+        .dayOfMonth((dayOfMonth) => dayOfMonth.list(5, 10, 15, 15))
+        .build();
       expect(expression.toString()).toBe("* * 5,10,15 * *");
     });
 
     it("should throw error for day of month out of range", () => {
       try {
-        new CronExpression().dayOfMonth(32).build();
+        new CronExpression()
+          .dayOfMonth((dayOfMonth) => dayOfMonth.value(0))
+          .build();
       } catch (e) {
         expect(e).toBeInstanceOf(DayOfMonthOutOfRangeError);
       }
@@ -105,28 +131,36 @@ describe(CronExpression, () => {
 
   describe("month", () => {
     it("should create every step month", () => {
-      const expression = new CronExpression().stepMonth(5).build();
+      const expression = new CronExpression()
+        .month((month) => month.step(5))
+        .build();
       expect(expression.toString()).toBe("* * * */5 *");
     });
 
     it("should create range month", () => {
-      const expression = new CronExpression().rangeMonth(5, 10).build();
+      const expression = new CronExpression()
+        .month((month) => month.range(5, 10))
+        .build();
       expect(expression.toString()).toBe("* * * 5-10 *");
     });
 
     it("should create list month", () => {
-      const expression = new CronExpression().month(5, 6, 7).build();
-      expect(expression.toString()).toBe("* * * 5,6,7 *");
+      const expression = new CronExpression()
+        .month((month) => month.list(5, 10, 11))
+        .build();
+      expect(expression.toString()).toBe("* * * 5,10,11 *");
     });
 
     it("should create list month with unique values", () => {
-      const expression = new CronExpression().month(5, 6, 7, 7).build();
-      expect(expression.toString()).toBe("* * * 5,6,7 *");
+      const expression = new CronExpression()
+        .month((month) => month.list(5, 10, 11, 11))
+        .build();
+      expect(expression.toString()).toBe("* * * 5,10,11 *");
     });
 
     it("should throw error for month out of range", () => {
       try {
-        new CronExpression().month(13).build();
+        new CronExpression().month((month) => month.value(13)).build();
       } catch (e) {
         expect(e).toBeInstanceOf(MonthOutOfRangeError);
       }
@@ -135,28 +169,38 @@ describe(CronExpression, () => {
 
   describe("dayOfWeek", () => {
     it("should create every step day of week", () => {
-      const expression = new CronExpression().stepDayOfWeek(5).build();
+      const expression = new CronExpression()
+        .dayOfWeek((dayOfWeek) => dayOfWeek.step(5))
+        .build();
       expect(expression.toString()).toBe("* * * * */5");
     });
 
     it("should create range day of week", () => {
-      const expression = new CronExpression().rangeDayOfWeek(5, 6).build();
-      expect(expression.toString()).toBe("* * * * 5-6");
+      const expression = new CronExpression()
+        .dayOfWeek((dayOfWeek) => dayOfWeek.range(5, 10))
+        .build();
+      expect(expression.toString()).toBe("* * * * 5-10");
     });
 
     it("should create list day of week", () => {
-      const expression = new CronExpression().dayOfWeek(5, 6, 0).build();
-      expect(expression.toString()).toBe("* * * * 5,6,0");
+      const expression = new CronExpression()
+        .dayOfWeek((dayOfWeek) => dayOfWeek.list(5, 6, 1))
+        .build();
+      expect(expression.toString()).toBe("* * * * 5,6,1");
     });
 
     it("should create list day of week with unique values", () => {
-      const expression = new CronExpression().dayOfWeek(5, 6, 0, 0).build();
-      expect(expression.toString()).toBe("* * * * 5,6,0");
+      const expression = new CronExpression()
+        .dayOfWeek((dayOfWeek) => dayOfWeek.list(5, 6, 1, 1))
+        .build();
+      expect(expression.toString()).toBe("* * * * 5,6,1");
     });
 
     it("should throw error for day of week out of range", () => {
       try {
-        new CronExpression().dayOfWeek(7).build();
+        new CronExpression()
+          .dayOfWeek((dayOfWeek) => dayOfWeek.value(7))
+          .build();
       } catch (e) {
         expect(e).toBeInstanceOf(DayOfWeekOutOfRangeError);
       }
@@ -166,13 +210,13 @@ describe(CronExpression, () => {
   describe("complex", () => {
     it("should create complex expression", () => {
       const expression = new CronExpression()
-        .stepMinute(5)
-        .rangeHour(5, 10)
-        .dayOfMonth(5, 10, 15)
-        .stepMonth(5)
-        .dayOfWeek(5, 6, 0)
+        .minute((minute) => minute.step(5))
+        .hour((hour) => hour.range(5, 10))
+        .dayOfMonth((dayOfMonth) => dayOfMonth.list(5, 10, 11))
+        .month((month) => month.step(5))
+        .dayOfWeek((dayOfWeek) => dayOfWeek.list(5, 6, 1))
         .build();
-      expect(expression.toString()).toBe("*/5 5-10 5,10,15 */5 5,6,0");
+      expect(expression).toBe("*/5 5-10 5,10,11 */5 5,6,1");
     });
   });
 });
