502679861c1f0dc8a1e4d036a0bd9829f65e4d96
handgarden <sjungwon03@gmail.com>
2024-03-24 00:20:55 +0900

    test: Add test case for returning unique values in ListCronField

--- test/expression/field/ListCronField.spec.ts
+++ test/expression/field/ListCronField.spec.ts
@@ -5,4 +5,9 @@ describe(ListCronField, () => {
     const field = new ListCronField([1, 2, 3]);
     expect(field.build()).toBe("1,2,3");
   });
+
+  it("should return unique values", () => {
+    const field = new ListCronField([1, 2, 2, 3, 3, 3]);
+    expect(field.build()).toBe("1,2,3");
+  });
 });
