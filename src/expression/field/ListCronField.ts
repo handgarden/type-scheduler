138194f1f56233860a96b8f7c5b66c5579a7da79
handgarden <sjungwon03@gmail.com>
2024-03-24 00:20:48 +0900

    fix: Refactor ListCronField to use uniqueValues method

--- src/expression/field/ListCronField.ts
+++ src/expression/field/ListCronField.ts
@@ -3,7 +3,11 @@ import { CronField } from "./CronField";
 export class ListCronField implements CronField {
   values: number[];
   constructor(values: number[]) {
-    this.values = values;
+    this.values = this.uniqueValues(values);
+  }
+
+  private uniqueValues(values: number[]): number[] {
+    return [...new Set(values)];
   }
 
   build(): string {
