6f85efb347d66daa000d29c48ff7628bd0ffd3c9
handgarden <sjungwon03@gmail.com>
2024-03-24 23:37:37 +0900

    test: Add JobMetadataArgs tests

--- test/metadata/JobMetadataArgs.spec.ts
+++ test/metadata/JobMetadataArgs.spec.ts
@@ -1,23 +1,69 @@
+import { JobHandler } from "../../src";
+import { ClassType } from "../../src/common/ClassType";
+import { ContainerRequiredError } from "../../src/error/ContainerRequiredError";
 import { JobMetadataArgs } from "../../src/metadata/JobMetadataArgs";
 
 describe(JobMetadataArgs, () => {
-  it("should create a new instance", () => {
-    const metadata = new JobMetadataArgs(() => {}, "0 0 * * *");
-    expect(metadata).toBeInstanceOf(JobMetadataArgs);
+  let target: ClassType<JobHandler>;
+  let cronExpression: string;
+  let name: string;
+  let token: string;
+
+  beforeEach(() => {
+    target = class implements JobHandler {
+      handle = jest.fn();
+    };
+    cronExpression = "* * * * *";
+    name = "test";
+    token = "token";
+  });
+  it("should return job metadata args", () => {
+    const args = new JobMetadataArgs({
+      target,
+      cronExpression,
+      name,
+      token,
+    });
+    expect(args.target).toBe(target);
+    expect(args.name).toBe(name);
+    expect(args.cronExpression).toBe(cronExpression);
+    expect(args.token).toBe(token);
   });
 
-  it("should have a constructor", () => {
-    const metadata = new JobMetadataArgs(() => {}, "0 0 * * *");
-    expect(metadata.constructor).toBeDefined();
-  });
+  describe("instantiateTarget", () => {
+    it("should return instance of target", () => {
+      const args = new JobMetadataArgs({
+        target,
+        cronExpression,
+      });
+      const instance = args.instantiateTarget();
+      expect(instance).toBeInstanceOf(target);
+    });
 
-  it("should have a cron expression", () => {
-    const metadata = new JobMetadataArgs(() => {}, "0 0 * * *");
-    expect(metadata.cronExpression).toBe("0 0 * * *");
-  });
+    it("should return instance of target from container", () => {
+      const container = {
+        get: jest.fn(),
+      };
+      const args = new JobMetadataArgs({
+        target,
+        cronExpression,
+        token,
+      });
+      args.instantiateTarget(container);
+      expect(container.get).toHaveBeenCalledWith(token);
+    });
 
-  it("should have a target", () => {
-    const metadata = new JobMetadataArgs(() => {}, "0 0 * * *");
-    expect(metadata.target).toBeDefined();
+    it("should throw an error if the container is undefined and the token is present.", () => {
+      const args = new JobMetadataArgs({
+        target,
+        cronExpression,
+        token,
+      });
+      try {
+        args.instantiateTarget();
+      } catch (e) {
+        expect(e).toBeInstanceOf(ContainerRequiredError);
+      }
+    });
   });
 });
