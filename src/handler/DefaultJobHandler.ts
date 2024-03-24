68f50e08d5e133ece557abf3acf6775568db6b87
handgarden <sjungwon03@gmail.com>
2024-03-24 23:42:47 +0900

    fix: Refactor DefaultJobHandler constructor and add name property

--- src/handler/DefaultJobHandler.ts
+++ src/handler/DefaultJobHandler.ts
@@ -1,15 +1,37 @@
 import { CronExpression } from "../expression";
+import { CronExpressionParser } from "../expression/CronExpressionParser";
 import { JobHandler } from "./JobHandler";
 
 export abstract class DefaultJobHandler implements JobHandler {
+  private readonly _name?: string;
   private readonly _cronExpression: CronExpression;
 
-  constructor(cronExpression: CronExpression) {
-    this._cronExpression = cronExpression;
+  constructor({
+    name,
+    cronExpression,
+  }: {
+    name?: string;
+    cronExpression: string | CronExpression;
+  }) {
+    this._name = name;
+    this._cronExpression = this.parseCronExpression(cronExpression);
+  }
+  private parseCronExpression(
+    cronExpression: CronExpression | string
+  ): CronExpression {
+    if (cronExpression instanceof CronExpression) {
+      return cronExpression;
+    }
+
+    return CronExpressionParser.parse(cronExpression);
   }
 
   public get cronExpression(): string {
     return this._cronExpression.build();
+  }
+
+  public get name(): string {
+    return this._name || this.constructor.name;
   }
 
   public abstract handle(): void | Promise<void>;
