116677b066d67ee22efe7b35dd86b1d847cf6c00
handgarden <sjungwon03@gmail.com>
2024-03-25 23:41:26 +0900

    test: Add JobHandlerMetadataArgs unit tests

---
+++ test/metadata/JobHandlerMetadataArgs.spec.ts
@@ -0,0 +1,69 @@
+import { JobHandler } from "../../src";
+import { ClassType } from "../../src/common/ClassType";
+import { ContainerRequiredError } from "../../src/error/ContainerRequiredError";
+import { JobHandlerMetadataArgs } from "../../src/metadata/JobHandlerMetadataArgs";
+
+describe(JobHandlerMetadataArgs, () => {
+  let target: ClassType<JobHandler>;
+  let cronExpression: string;
+  let name: string;
+  let token: string;
+
+  beforeEach(() => {
+    target = class implements JobHandler {
+      handle = jest.fn();
+    };
+    cronExpression = "* * * * *";
+    name = "test";
+    token = "token";
+  });
+  it("should return job metadata args", () => {
+    const args = new JobHandlerMetadataArgs({
+      target,
+      cronExpression,
+      name,
+      token,
+    });
+    expect(args.target).toBe(target);
+    expect(args.name).toBe(name);
+    expect(args.cronExpression).toBe(cronExpression);
+    expect(args.token).toBe(token);
+  });
+
+  describe("instantiateTarget", () => {
+    it("should return instance of target", () => {
+      const args = new JobHandlerMetadataArgs({
+        target,
+        cronExpression,
+      });
+      const instance = args.instantiateTarget();
+      expect(instance).toBeInstanceOf(target);
+    });
+
+    it("should return instance of target from container", () => {
+      const container = {
+        get: jest.fn(),
+      };
+      const args = new JobHandlerMetadataArgs({
+        target,
+        cronExpression,
+        token,
+      });
+      args.instantiateTarget(container);
+      expect(container.get).toHaveBeenCalledWith(token);
+    });
+
+    it("should throw an error if the container is undefined and the token is present.", () => {
+      const args = new JobHandlerMetadataArgs({
+        target,
+        cronExpression,
+        token,
+      });
+      try {
+        args.instantiateTarget();
+      } catch (e) {
+        expect(e).toBeInstanceOf(ContainerRequiredError);
+      }
+    });
+  });
+});
