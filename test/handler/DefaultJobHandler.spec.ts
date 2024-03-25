01bb5c1b7140d3296b59db2103dacf99ec1cace3
handgarden <sjungwon03@gmail.com>
2024-03-26 00:18:30 +0900

    fix: Refactor DefaultJobHandler.spec.ts

--- test/handler/DefaultJobHandler.spec.ts
+++ test/handler/DefaultJobHandler.spec.ts
@@ -1,47 +1,54 @@
 import { CronExpression } from "../../src/expression";
 import { DefaultJobHandler } from "../../src/handler/DefaultJobHandler";
 
-function mockedJobHandler(expression: CronExpression): DefaultJobHandler {
+function mockedJobHandler(
+  expression: CronExpression | string
+): DefaultJobHandler {
   const mock = class MockedJobHandler extends DefaultJobHandler {
+    constructor(expression: CronExpression | string) {
+      super({
+        cronExpression: expression,
+        name: "test",
+      });
+    }
     public handle(): void {}
   };
   return new mock(expression);
 }
 
 describe(DefaultJobHandler, () => {
-  let expression: CronExpression;
+  let expression: CronExpression | string;
   let handler: DefaultJobHandler;
-
-  beforeEach(() => {
-    expression = {
-      build: jest.fn(),
-    } as unknown as CronExpression;
-    handler = mockedJobHandler(expression);
-  });
 
   describe("constructor", () => {
     it("should create a new instance", () => {
+      handler = mockedJobHandler("0 0 * * *");
+
       expect(handler).toBeInstanceOf(DefaultJobHandler);
+    });
+  });
+
+  describe("string expression", () => {
+    it("should return the string expression", () => {
+      expression = "0 0 * * *";
+      handler = mockedJobHandler(expression);
+      expect(handler.cronExpression).toBe("0 0 * * *");
+    });
+
+    it("should throw an error if the expression is not a valid cron expression", () => {
+      expression = "0 0 * * * *";
+      expect(() => mockedJobHandler(expression)).toThrow();
     });
   });
 
   describe("cronExpression", () => {
     it("should return the cron expression", () => {
-      expression.build = jest.fn().mockReturnValue("0 0 * * *");
+      expression = new CronExpression()
+        .minute((minute) => minute.value(0))
+        .hour((hour) => hour.value(0))
+        .build();
+      handler = mockedJobHandler(expression);
       expect(handler.cronExpression).toBe("0 0 * * *");
-    });
-
-    it("should call the build method", () => {
-      handler.cronExpression;
-      expect(expression.build).toHaveBeenCalled();
-    });
-  });
-
-  describe("handle", () => {
-    it("should call the handle method", () => {
-      handler.handle = jest.fn();
-      handler.handle();
-      expect(handler.handle).toHaveBeenCalled();
     });
   });
 });
