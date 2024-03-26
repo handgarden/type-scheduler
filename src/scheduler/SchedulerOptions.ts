d371f778808aa70a15c5367a5746013f7d3b6e21
handgarden <sjungwon03@gmail.com>
2024-03-27 00:23:21 +0900

    feat: Add support for jobs in SchedulerOptions constructor

--- src/scheduler/SchedulerOptions.ts
+++ src/scheduler/SchedulerOptions.ts
@@ -1,12 +1,19 @@
-import { Container } from "../common";
+import { ClassType, Container } from "../common";
+import { JobHandler } from "../handler";
 import { ScheduleRunner } from "./ScheduleRunner";
 
 export class SchedulerOptions {
   public runner: ScheduleRunner;
   public container?: Container;
+  public jobs?: ClassType<JobHandler>[];
 
-  constructor(runner: ScheduleRunner, container?: Container) {
+  constructor(
+    runner: ScheduleRunner,
+    container?: Container,
+    jobs?: ClassType<JobHandler>[]
+  ) {
     this.runner = runner;
     this.container = container;
+    this.jobs = jobs;
   }
 }
