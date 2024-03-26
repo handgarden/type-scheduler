e52ae8152e5ee5d09b2bb68710cc6825f7442e3c
handgarden <sjungwon03@gmail.com>
2024-03-26 21:20:36 +0900

    test: Refactor SchedulerOptions.spec.ts

--- test/scheduler/SchedulerOptions.spec.ts
+++ test/scheduler/SchedulerOptions.spec.ts
@@ -1,31 +1,18 @@
-import { SchedulerOptions } from "../../src/scheduler/SchedulerOptions";
+import { SchedulerOptions, ScheduleRunner } from "../../src";
+import { Container } from "../../src/common";
 
 describe(SchedulerOptions, () => {
-  describe("isEnabled", () => {
-    it("should return true if the scheduler is enabled", () => {
-      const options = new SchedulerOptions({
-        enabled: true,
-        runner: { schedule: () => {} },
-      });
-      expect(options.isEnabled()).toBeTruthy();
-    });
-    it("should return false if the scheduler is disabled", () => {
-      const options = new SchedulerOptions({
-        enabled: false,
-        runner: { schedule: () => {} },
-      });
-      expect(options.isEnabled()).toBeFalsy();
-    });
-  });
-
-  describe("getRunner", () => {
-    it("should return the runner", () => {
-      const runner = { schedule: () => {} };
-      const options = new SchedulerOptions({
-        enabled: true,
-        runner,
-      });
-      expect(options.getRunner()).toBe(runner);
+  describe("constructor", () => {
+    it("should set runner and container", () => {
+      const runner: ScheduleRunner = {
+        schedule: jest.fn(),
+      };
+      const container: Container = {
+        get: jest.fn(),
+      };
+      const options = new SchedulerOptions(runner, container);
+      expect(options.runner).toBe(runner);
+      expect(options.container).toBe(container);
     });
   });
 });
